4f525a62da14cc11ac2633e8aac2295c296df2b4
JorgeMira <jorge.mira.yague@gmail.com>
2024-12-04 21:03:52 +0400

    Create test cases for `flatten_dict` function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,26 @@
-from datura.main import main
+from typing import Any
+
+import pytest
+
+from datura.flatten_dict import flatten_dict
 
 
-def test_main():
-    assert main() is None
+@pytest.mark.parametrize("data", [None, 1, "a", []])
+def test_flatten_dict_invalid(data: Any) -> None:
+    with pytest.raises(ValueError):
+        flatten_dict(data)
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ({}, {}),  # Empty value
+        ({"a": 1, "b": 2, "c": 3}, {"a": 1, "b": 2, "c": 3}),  # Simple example
+        (  # Provided test case
+            {"a": 1, "b": {"c": 2, "d": {"e": 3, "f": 4}}, "g": {"h": 5}},
+            {"a": 1, "b.c": 2, "b.d.e": 3, "b.d.f": 4, "g.h": 5},
+        ),
+    ],
+)
+def test_flatten_dict_valid(value: dict, expected: dict) -> None:
+    assert flatten_dict(value) == expected
